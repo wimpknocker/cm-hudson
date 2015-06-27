6a81b8b230652f7fb9de39d72e540e68c43387d3
wimpknocker@wimpnether <wimpknocker@wimpnether.net>
2015-06-27 23:11:21 +0300

    patch.sh

---
+++ patcher.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+PATCHDIR=$WORKSPACE/patches/$REPO_BRANCH
+PATCHTO=$WORKSPACE/$JENKINS_BUILD_DIR
+UNATTENDED=${1}
+
+echo $PATCHTO
+cd $PATCHDIR
+for LINE in $(find -name *.patch | sort )
+do
+    if [[ $UNATTENDED -ne 1 ]]; then
+      clear
+    fi
+    echo "patch = $PATCHDIR/$LINE"
+    PATCH=$PATCHDIR/$LINE
+    REPO=$(dirname $LINE)
+    echo "repo = $REPO"
+    cd $PATCHDIR
+    cd $REPO
+    RESULT=$(patch -p1 --no-backup-if-mismatch < $PATCH)
+    echo -e "${RESULT}"
+    if [[ $(echo $RESULT | grep -c FAILED) -gt 0 ]]; then
+      echo "Fail!"
+      if [[ $UNATTENDED -eq 1 ]]; then
+        exit 9
+      else
+        read -p "Patch Failed!" yn
+        break;
+      fi
+    fi
+    if [[ $(echo $RESULT | grep -c "saving rejects to file") -gt 0 ]]; then
+      echo "Fail!"
+      echo "Fix the patch!"
+      if [[ $UNATTENDED -eq 1 ]]; then
+        exit 9
+      else
+        read -p "Patch Rejects!" yn
+        break;
+      fi
+    fi
+    if [[ $(echo $RESULT | grep -c "Skip this patch") -gt 0 ]]; then
+      echo "Fail!"
+      echo "Fix the patch"
+      if [[ $UNATTENDED -eq 1 ]]; then
+        exit 9
+      else
+        read -p "Patch Skipped!" yn
+        break;
+      fi
+    fi
+    cd $PATCHTO
+done
+
+cd $PATCHDIR
+for LINE in $(find -name *.apply | sort )
+do
+    if [[ $UNATTENDED -ne 1 ]]; then
+      clear
+    fi
+    echo "patch = $PATCHDIR/$LINE"
+    PATCH=$PATCHDIR/$LINE
+    REPO=$(dirname $LINE)
+    echo "repo = $REPO"
+    cd $PATCHDIR
+    cd $REPO
+    RESULT=$(git apply --whitespace=nowarm -v $PATCH 2>$1)
+    echo -e "${RESULT}"
+    if [[ $(echo $RESULT | grep -c error:) -gt 0 ]]; then
+      echo "Fail!"
+      echo "Fix the patch!"
+      if [[ $UNATTENDED -eq 1 ]]; then
+        exit 9
+      else
+        read -p "Patch Error!" yn
+        break;
+      fi
+    fi
+    cd $PATCHTO
+done
